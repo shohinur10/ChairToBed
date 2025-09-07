cee6c6b1f9752fd423542f733bc202488cc65957
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-09-07 18:40:53 +0900

    feat:deploy project via shell & pm2

---
+++ process.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps : [{
+      name   : "ChairToBed",
+      script : "./dist/server.js",
+      watch:false,
+      env_production: {
+         NODE_ENV: "production"
+      },
+      env_development: {
+         NODE_ENV: "development"
+      },
+      instances:1,
+      exec_mode:"cluster"
+    }]
+  }
---
+++ train.ts
@@ -0,0 +1,10 @@
+/* 
+PM@ COMMANDS:
+
+pm2 ls
+pm2  start dist/server.js --name=ChairToBed
+pm2 start"npm run start:prod" --name =ChairToBed
+pm2 stop id
+pm2 delete id
+pm2 restart id
+*/
